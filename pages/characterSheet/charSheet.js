cc304d508c058071e6ff8215b4beb0e53f12dbfb
LujanSolo <lujansolo@proton.me>
2024-01-04 21:49:07 -0800

    charSheet html initial layout started

---
+++ pages/characterSheet/charSheet.js
@@ -0,0 +1,8 @@
+// const ageSelection = document.getElementById("character-age");
+
+// ageSelection[0] = new Option("Select Age", 0);
+// for(let i = 1; i < 901; i++){
+//   ageSelection[i] = new Option(i, i);
+// };
+
+// document.body.appendChild(ageSelection);

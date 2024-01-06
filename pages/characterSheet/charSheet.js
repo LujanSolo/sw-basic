99380bd58e71c054068a3f174f15c8acf1af5d13
LujanSolo <lujansolo@proton.me>
2024-01-05 20:56:11 -0800

    restyled character details section. built attributesAndSkills object on js file

--- pages/characterSheet/charSheet.js
+++ pages/characterSheet/charSheet.js
@@ -6,3 +6,240 @@
 // };
 
 // document.body.appendChild(ageSelection);
+
+const attributesAndSkills = {
+  dexterity: {
+    name: "Dexterity",
+    value: 0,
+    pips: 0,
+    skills: {
+      blaster: {
+        name: "Blaster",
+        value: 0,
+      },
+      brawling: {
+        name: "Brawling",
+        value: 0,
+      },
+      brawlingParry: {
+        name: "Brawling Parry",
+        value: 0,
+      },
+      dodge: {
+        name: "Dodge",
+        value: 0,
+      },
+      grenade: {
+        name: "Grenade",
+        value: 0,
+      },
+      heavyWeapons: {
+        name: "Heavy Weapons",
+        value: 0,
+      },
+      meleeParry: {
+        name: "Melee Parry",
+        value: 0,
+      },
+      melee: {
+        name: "Melee",
+        value: 0,
+      },
+    }
+  },
+  perception: {
+    name: "Perception",
+    value: 0,
+    pips: 0,
+    skills: {
+      bargain: {
+        name: "Bargain",
+        value: 0,
+      },
+      command: {
+        name: "Command",
+        value: 0,
+      },
+      con: {
+        name: "Con",
+        value: 0,
+      },
+      dodge: {
+        name: "Dodge",
+        value: 0,
+      },
+      grenade: {
+        name: "Grenade",
+        value: 0,
+      },
+      heavyWeapons: {
+        name: "Heavy Weapons",
+        value: 0,
+      },
+      meleeParry: {
+        name: "Melee Parry",
+        value: 0,
+      },
+      melee: {
+        name: "Melee",
+        value: 0,
+      },
+    }
+  },
+  dexterity: {
+    name: "Dexterity",
+    value: 0,
+    pips: 0,
+    skills: {
+      blaster: {
+        name: "Blaster",
+        value: 0,
+      },
+      brawling: {
+        name: "Brawling",
+        value: 0,
+      },
+      con: {
+        name: "Con",
+        value: 0,
+      },
+      gambling: {
+        name: "Gambling",
+        value: 0,
+      },
+      grenade: {
+        name: "Grenade",
+        value: 0,
+      },
+      heavyWeapons: {
+        name: "Heavy Weapons",
+        value: 0,
+      },
+      meleeParry: {
+        name: "Melee Parry",
+        value: 0,
+      },
+      melee: {
+        name: "Melee",
+        value: 0,
+      },
+    }
+  },
+  dexterity: {
+    name: "Dexterity",
+    value: 0,
+    pips: 0,
+    skills: {
+      blaster: {
+        name: "Blaster",
+        value: 0,
+      },
+      brawling: {
+        name: "Brawling",
+        value: 0,
+      },
+      brawlingParry: {
+        name: "Brawling Parry",
+        value: 0,
+      },
+      dodge: {
+        name: "Dodge",
+        value: 0,
+      },
+      grenade: {
+        name: "Grenade",
+        value: 0,
+      },
+      heavyWeapons: {
+        name: "Heavy Weapons",
+        value: 0,
+      },
+      meleeParry: {
+        name: "Melee Parry",
+        value: 0,
+      },
+      melee: {
+        name: "Melee",
+        value: 0,
+      },
+    }
+  },
+  dexterity: {
+    name: "Dexterity",
+    value: 0,
+    pips: 0,
+    skills: {
+      blaster: {
+        name: "Blaster",
+        value: 0,
+      },
+      brawling: {
+        name: "Brawling",
+        value: 0,
+      },
+      brawlingParry: {
+        name: "Brawling Parry",
+        value: 0,
+      },
+      dodge: {
+        name: "Dodge",
+        value: 0,
+      },
+      grenade: {
+        name: "Grenade",
+        value: 0,
+      },
+      heavyWeapons: {
+        name: "Heavy Weapons",
+        value: 0,
+      },
+      meleeParry: {
+        name: "Melee Parry",
+        value: 0,
+      },
+      melee: {
+        name: "Melee",
+        value: 0,
+      },
+    }
+  },
+  dexterity: {
+    name: "Dexterity",
+    value: 0,
+    pips: 0,
+    skills: {
+      blaster: {
+        name: "Blaster",
+        value: 0,
+      },
+      brawling: {
+        name: "Brawling",
+        value: 0,
+      },
+      brawlingParry: {
+        name: "Brawling Parry",
+        value: 0,
+      },
+      dodge: {
+        name: "Dodge",
+        value: 0,
+      },
+      grenade: {
+        name: "Grenade",
+        value: 0,
+      },
+      heavyWeapons: {
+        name: "Heavy Weapons",
+        value: 0,
+      },
+      meleeParry: {
+        name: "Melee Parry",
+        value: 0,
+      },
+      melee: {
+        name: "Melee",
+        value: 0,
+      },
+    }
+  },
+};
